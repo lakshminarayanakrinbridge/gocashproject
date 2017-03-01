b548c56b9c6114dec7c9f1d45ed04ef58ddf1ded
Lakshminarayana K R <lakshminarayana.inbridge@gmail.com>
2017-03-01 12:11:33 +0530

    This is commited on 1/3/2017

---
+++ app/src/main/java/www/com/inbridge/ecash24012017/Myapp.java
@@ -0,0 +1,15 @@
+package www.com.inbridge.ecash24012017;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by USER on 3/1/2017.
+ */
+
+public class Myapp extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
