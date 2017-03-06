123e21dfd93d733490e96144d6010610ff50131e
Lakshminarayana K R <lakshminarayana.inbridge@gmail.com>
2017-03-06 18:11:39 +0530

    This is commited on 6/3/2017

--- app/src/main/java/www/com/inbridge/ecash24012017/LoginActivity.java
+++ app/src/main/java/www/com/inbridge/ecash24012017/LoginActivity.java
@@ -199,8 +199,9 @@ public class LoginActivity extends AppCompatActivity {
                                 } else {
                                     //If the server response is not success
                                     //Displaying an error message on toast
+                                    progressBar.setVisibility(View.GONE);
                                     Toast.makeText(LoginActivity.this, "Invalid username or password", Toast.LENGTH_SHORT).show();
-                                    progressBar.setVisibility(View.INVISIBLE);
+
                                 }
                             }
                             //Starting profile activity
--- app/src/main/java/www/com/inbridge/ecash24012017/Myapp.java
+++ app/src/main/java/www/com/inbridge/ecash24012017/Myapp.java
@@ -1,11 +1,9 @@
 package www.com.inbridge.ecash24012017;
 
-import android.support.multidex.MultiDexApplication;
-
 /**
  * Created by USER on 3/1/2017.
  */
-
+/*
 public class Myapp extends MultiDexApplication {
 
     @Override
@@ -13,3 +11,4 @@ public class Myapp extends MultiDexApplication {
         super.onCreate();
     }
 }
+*/
